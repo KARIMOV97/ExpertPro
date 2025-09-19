75680a4b4a48b098f16e3b17d296de0eeb403191
San'atbek <sanatbekkarimov@gmail.com>
2025-09-19 08:51:56 +0500

    created superadmin

--- form/views.py
+++ form/views.py
@@ -238,6 +238,7 @@ class KuzovTuriYemirilishViewSet(viewsets.ModelViewSet):
 
 
 class TanlanganDetalViewSet(viewsets.ModelViewSet):
+    TanlanganDetal.objects.all().delete()
     serializer_class = TanlanganDetalSerializer
     queryset = TanlanganDetal.objects.all()
     
